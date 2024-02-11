a469cb3a99fe9dfa9787e3a33dc9e48c6af9e918
Aashir <aashirnawaz5@gmail.com>
2024-02-11 21:11:45 +0500

    updated

--- src/data.js
+++ src/data.js
@@ -1,35 +1,35 @@
 const Data=[
     {
         title:'Model 3',
-        url_sm:'/model3_sm.jpg',
-        url_lg:'/model3_lg.jpg',
+        url_sm:'model3_sm.jpg',
+        url_lg:'model3_lg.jpg',
         design:"https://www.tesla.com/model3/design"
 
 
     },
     {
         title:'Model X',
-        url_sm:'/teslaX_sm.jpeg',
-        url_lg:'/teslaX_lg.jpeg',
+        url_sm:'teslaX_sm.jpeg',
+        url_lg:'teslaX_lg.jpeg',
         design:"https://www.tesla.com/modelx/design#overview"
     },
     {
         title:'Model S',
-        url_sm:'/teslaS_sm.jpg',
-        url_lg:'/teslaS_lg.jpg',
+        url_sm:'teslaS_sm.jpg',
+        url_lg:'teslaS_lg.jpg',
         design:"https://www.tesla.com/models/design#overview"
     },
 
     {
         title:'Tesla Solar Roof',
-        url_sm:'/teslaSolar_sm.jpg',
-        url_lg:'/teslaSolar_lg.jpeg',
+        url_sm:'teslaSolar_sm.jpg',
+        url_lg:'teslaSolar_lg.jpeg',
         design:'https://www.tesla.com/energy/design'
     },
     {
         title:'Tesla Cyber Truck',
-        url_sm:'/teslaTruck_sm.jpg',
-        url_lg:'/teslaTruck_lg.jpg',
+        url_sm:'teslaTruck_sm.jpg',
+        url_lg:'teslaTruck_lg.jpg',
         design:'https://www.tesla.com/cybertruck/design#payment'
     }
 ]
